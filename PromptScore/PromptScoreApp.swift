bbf9937d5cf5e03d30f8834101b636ea262f8a43
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-05-30 09:24:01 +0100

    Initial Commit

---
+++ PromptScore/PromptScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptScoreApp.swift
+//  PromptScore
+//
+//  Created by Aidan O'Brien on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PromptScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptScoreTests/PromptScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromptScoreTests.swift
+//  PromptScoreTests
+//
+//  Created by Aidan O'Brien on 30/05/2025.
+//
+
+import Testing
+@testable import PromptScore
+
+struct PromptScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
